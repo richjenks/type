5916b25ff25025a967814ec9698ea0a501e6d6fe
rich@richjenks.com <rich@richjenks.com>
2014-10-31 14:07:32 +0000

    Enabled use of tab key

---
+++ script.js
@@ -0,0 +1,20 @@
+// Get stored text
+if (localStorage.getItem("type")) {
+	document.getElementById("type").value = localStorage.getItem("type");
+}
+
+// Enable tab key - see http://jsfiddle.net/2wAzx/13/
+function enableTab(id) {
+	var el = document.getElementById(id);
+	el.onkeydown = function(e) {
+		if (e.keyCode === 9) {
+			var val = this.value,
+				start = this.selectionStart,
+				end = this.selectionEnd;
+			this.value = val.substring(0, start) + '\t' + val.substring(end);
+			this.selectionStart = this.selectionEnd = start + 1;
+			return false;
+		}
+	};
+}
+enableTab("type");
