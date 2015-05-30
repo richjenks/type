bb3468ee0206eca6927fe02b6826837703ea4828
Rich Jenks <rich@richjenks.com>
2015-05-30 12:25:38 +0100

    Added throttling on save to 250ms

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-// Get stored text
-if (localStorage.getItem("type")) {
-	document.getElementById("type").value = localStorage.getItem("type");
-}
+// Typing area
+var type = document.getElementById("type");
 
-// Save content
-function save(text) {
+// Save text
+var save = function (text) {
 	localStorage.setItem('type', text);
 	document.title = text.split('\n')[0];
 }
 
 // Enable tab key - see http://jsfiddle.net/2wAzx/13/
-function enableTab(id) {
+var enableTab = function (id) {
 	var el = document.getElementById(id);
 	el.onkeydown = function(e) {
 		if (e.keyCode === 9) {
@@ -23,4 +21,29 @@ function enableTab(id) {
 		}
 	};
 }
+
+// Prevent saving on each keypress - see https://remysharp.com/2010/07/21/throttling-function-calls
+var debounce = function (fn, delay) {
+	var timer = null;
+	return function () {
+		var context = this, args = arguments;
+		clearTimeout(timer);
+		timer = setTimeout(function () {
+			fn.apply(context, args);
+		}, delay);
+	};
+}
+
+// Restore saved text
+if (localStorage.getItem("type")) {
+	type.value = localStorage.getItem("type");
+	save(type.value);
+}
+
+// Save content
+type.addEventListener("keyup", debounce(function () {
+	save(this.value);
+}, 250));
+
+// Enable tab key
 enableTab("type");
